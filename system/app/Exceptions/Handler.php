74aa4d2c7d12287b4e81bd9cbf50c48cfe5b96e0
Eduardo Gonçalves Alexandretti <Dudu@MacBook-Pro-de-Eduardo.local>
2024-09-01 02:50:14 -0300

    adicionado personalizacao de resposta

--- system/app/Exceptions/Handler.php
+++ system/app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,6 +25,12 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        $this->renderable(function (AccessDeniedHttpException $e, Request $request) {
+            return response()->json([
+                "message" => "unauthenticated.",
+            ], 403);
+        });
+
         $this->reportable(function (Throwable $e) {
             //
         });
